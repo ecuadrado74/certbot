89f84832d9a021c2ee7f1ceb49da38e4e2067ba3
Emi <emi@entorno.es>
2020-11-20 14:10:51 +0100

    se anyade lo minimo

--- certbot/examples/plugins/certbot_example_plugins.py
+++ certbot/examples/plugins/certbot_example_plugins.py
@@ -18,6 +18,40 @@ class Authenticator(common.Plugin):
 
     # Implement all methods from IAuthenticator, remembering to add
     # "self" as first argument, e.g. def prepare(self)...
+    def __init__(self, *args, **kwargs):
+        super(Authenticator, self).__init__(*args, **kwargs)
+        self.credentials = None
+
+    @classmethod
+    def more_info(self):  # pylint: disable=missing-docstring,no-self-use
+        return (
+            "This plugin configures a DNS TXT record to respond to a dns-01 challenge using "
+            + "the EuroDNS Remote REST API."
+        )
+
+    def prepare(self):  # pylint: disable=missing-docstring,no-self-use
+        return (
+            "This plugin configures a DNS TXT record to respond to a dns-01 challenge using "
+            + "the EuroDNS Remote REST API."
+        )
+
+    def get_chall_pref(self,domain):  # pylint: disable=missing-docstring,no-self-use
+        return (
+            "This plugin configures a DNS TXT record to respond to a dns-01 challenge using "
+            + "the EuroDNS Remote REST API."
+        )
+
+    def perform(self,achalls):  # pylint: disable=missing-docstring,no-self-use
+        return (
+            "This plugin configures a DNS TXT record to respond to a dns-01 challenge using "
+            + "the EuroDNS Remote REST API."
+        )
+
+    def cleanup(self,achalls):  # pylint: disable=missing-docstring,no-self-use
+        return (
+            "This plugin configures a DNS TXT record to respond to a dns-01 challenge using "
+            + "the EuroDNS Remote REST API."
+        )
 
 
 @zope.interface.implementer(interfaces.IInstaller)
