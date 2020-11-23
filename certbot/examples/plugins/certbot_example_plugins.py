f1d4674cf4db8ae80c2639ee1ea221cfa143501a
Emi <emi@entorno.es>
2020-11-23 15:18:47 +0100

    tmp1

--- certbot/examples/plugins/certbot_example_plugins.py
+++ certbot/examples/plugins/certbot_example_plugins.py
@@ -3,15 +3,20 @@
 For full examples, see `certbot.plugins`.
 
 """
+
+import logging
+
 import zope.interface
 
 from certbot import interfaces
 from certbot.plugins import common
+from certbot.plugins import dns_common
 
+logger = logging.getLogger(__name__)
 
 @zope.interface.implementer(interfaces.IAuthenticator)
 @zope.interface.provider(interfaces.IPluginFactory)
-class Authenticator(common.Plugin):
+class Authenticator(dns_common.DNSAuthenticator):
     """Example Authenticator."""
 
     description = "Example Authenticator plugin"
@@ -23,6 +28,23 @@ class Authenticator(common.Plugin):
         self.credentials = None
 
     @classmethod
+    def add_parser_arguments(cls, add):  # pylint: disable=arguments-differ
+        super(Authenticator, cls).add_parser_arguments(
+            add, default_propagation_seconds=120
+        )
+        add("credentials", help="ISPConfig credentials INI file.")
+
+    def _setup_credentials(self):
+        self.credentials = self._configure_credentials(
+            "credentials",
+            "ISPConfig credentials INI file",
+            {
+                "endpoint": "entorno.es",
+                "username": "emiprueba",
+                "password": "emipassword",
+            },
+        )
+
     def more_info(self):  # pylint: disable=missing-docstring,no-self-use
         return (
             "This plugin configures a DNS TXT record to respond to a dns-01 challenge using "
